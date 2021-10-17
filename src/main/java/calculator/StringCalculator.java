3b7783e27cf6b2e7a818bc1419314ed1669511b0
vaibhav <vaibhavmhaske023@gmail.com>
2021-10-17 21:44:16 +0530

    added string calculation functionalities with testcases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -14,4 +14,83 @@ class StringCalculator {
 		customDelimiter = "";
 	}
 
+	public String add(String numbers) {
+
+		if (numbers.isEmpty())
+			return String.format("%.0f", res);
+
+		if (isInvalidLastCharacterIn(numbers))
+			return "Number expected but EOF found.";
+
+		if (numbers.startsWith(CUSTOM_DELIMITER_PREFIX))
+			numbers = setCustomDelimiter(numbers);
+
+		if (isNewlineAtInvalidPositionIn(numbers))
+			return String.format("Number expected but '\n' found at position %d.", numbers.lastIndexOf('\n'));
+
+		if (containsNegative(numbers).length() > 0)
+			return String.format("Negative numbers not allowed: %s", containsNegative(numbers));
+		calculateSumOf(getStringArray(numbers));
+		return hasDecimalPlaces() ? printFloat() : printInteger();
+
+	}
+	private boolean isInvalidLastCharacterIn(String numbers) {
+		return Character.digit(numbers.charAt(numbers.length() - 1), 10) < 0;
+	}
+
+
+	private boolean isNewlineAtInvalidPositionIn(String numbers) {
+		return numbers.lastIndexOf(NEWLINE) > numbers.lastIndexOf(DEFAULT_DELIMITER);
+	}
+
+	private StringBuilder containsNegative(String numbers) {
+		StringBuilder negativeNumbers = new StringBuilder();
+
+		for (String number : getStringArray(numbers))
+			if (Float.valueOf(number) < 0) negativeNumbers.append(number + ",");
+
+		boolean commaIsLastChar = negativeNumbers.length() > 0 && negativeNumbers.charAt(negativeNumbers.length() -1) == ',';
+
+		return commaIsLastChar ? negativeNumbers.deleteCharAt(negativeNumbers.length() - 1)
+				: negativeNumbers;
+	}
+
+	private String setCustomDelimiter(String numbers) {
+		int customDelimiterStart = numbers.lastIndexOf(CUSTOM_DELIMITER_PREFIX) + 1;
+		int customDelimiterEnd = numbers.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+		customDelimiter = numbers.substring(customDelimiterStart, customDelimiterEnd);
+
+		return numbers.substring(customDelimiterEnd + 1).replace(customDelimiter, DEFAULT_DELIMITER);
+	}
+
+	private String[] getStringArray(String numbers) {
+		return numbers.replace(NEWLINE, DEFAULT_DELIMITER).split(DEFAULT_DELIMITER);
+	}
+
+	private void calculateSumOf(String[] numbers) {
+		String m="";
+		for (String number : numbers) {
+			if( Float.parseFloat(number)>1000) {
+				m="0";
+			}
+			else {
+				m=number;
+			}
+			res = Float.sum(res, Float.parseFloat(m));
+		}
+	}
+
+	private boolean hasDecimalPlaces() {
+		return res % 1 != 0;
+	}
+
+	private String printFloat() {
+		return Float.toString((float) (Math.round(res * 100.0) / 100.0));
+	}
+
+	private String printInteger() {
+		return String.valueOf((int) res);
+
+	}
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-//    @Test
-//    void empty_string_should_return_0() {
-//        StringCalculator stringCalculator = new StringCalculator();
-//        assertEquals(0, stringCalculator.add(""));
-//    }
-//
-//    @Test
-//    void string_with_single_number_should_return_number_as_int() {
-//        StringCalculator stringCalculator = new StringCalculator();
-//        assertEquals(1, stringCalculator.add("1"));
-//    }
+	 @Test
+	    public void
+	    return_0_when_input_is_empty() {
+	        assertEquals("0", calculate(""));
+	    }
+
+	    @Test
+	    public void
+	    return_3_when_input_is_1_2() {
+	        assertEquals("3", calculate("1,2"));
+	    }
+	    
+	    @Test
+	    public void
+	    return_3_when_input_is_any_numbers() {
+	        assertEquals("21", calculate("1,2,3,4,5,6"));
+	    }
+	    
+	    @Test
+	    public void
+	    return_3_when_input_is_for_1001() {
+	        assertEquals("20", calculate("1001,2,3,4,5,6"));
+	    }
+
+	    @Test
+	    public void
+	    sum_floats_and_return_float() {
+	        assertEquals("6.6", calculate("2.2,4.4"));
+	    }
+
+	    @Test
+	    public void
+	    treat_newLine_as_a_delimiter() {
+	        assertEquals("6", calculate("1\n2,3"));
+	    }
+
+	    @Test
+	    public void
+	    return_error_msg_when_newLine_at_invalid_position() {
+	        assertEquals("Number expected but '\n' found at position 6.", calculate("1,2,5,\n3"));
+	    }
+
+	    @Test
+	    public void
+	    return_error_msg_when_delimiter_at_last_position() {
+	        assertEquals("Number expected but EOF found.", calculate("2,3,4.2,"));
+	    }
+
+	    @Test
+	    public void
+	    return_correct_sum_when_custom_delimiter_is_used() {
+	        assertEquals("3", calculate("//;\n1;2"));
+	        assertEquals("3", calculate("//|\n1|2"));
+	        assertEquals("8", calculate("//##\n1##2##5"));
+	        assertEquals("5", calculate("//fe\n2fe3"));
+	    }
+
+	    @Test
+	    public void
+	    return_string_of_negative_numbers_when_negative_numbers_are_used_as_input() {
+	        assertEquals("Negative numbers not allowed: -1", calculate("-1,2"));
+	        assertEquals("Negative numbers not allowed: -4,-5", calculate("2,-4,-5"));
+	    }
+
+	    private String calculate(String number) {
+	        StringCalculator stringCalculator = new StringCalculator();
+	        return stringCalculator.add(number);
+	    }
+
 }
