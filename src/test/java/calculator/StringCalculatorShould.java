f58632d35d3d84354ca236f3e8137077d7b9f67f
LOV <LOV@DESKTOP-89BB5UJ>
2021-10-17 15:15:19 +0530

    negative number

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,24 +1,26 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
 
+@Testable
 class StringCalculatorShould {
 	
 	
     @Test
     void empty_string_should_return_0() throws Exception {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        StringCalculator cal = new StringCalculator();
+        assertEquals(0, cal.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() throws Exception {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        StringCalculator cal = new StringCalculator();
+        assertEquals(1, cal.add("1"));
     }
     
     @Test
@@ -31,8 +33,21 @@ class StringCalculatorShould {
     @Test
 	public void twoNumbersNewLineReturnSum() throws Exception
 	{
-    	StringCalculator stringCalculator = new StringCalculator();
+    	StringCalculator cal = new StringCalculator();
 
-		assertEquals(stringCalculator.add("1\n2"), 3);
+		assertEquals(cal.add("1\n2"), 3);
+	}
+    
+    @Test
+	public void threeNumberDelimitedBothWayReturnSum() throws Exception {
+    	
+    	StringCalculator cal = new StringCalculator();
+		assertEquals(cal.add("1,2,3"), 6);
+	}
+    
+    @Test
+	public void negativeReturnsException() throws Exception {
+    	StringCalculator cal = new StringCalculator();
+		cal.add("-1");
 	}
 }
