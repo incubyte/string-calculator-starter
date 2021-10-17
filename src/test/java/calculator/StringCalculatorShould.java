9ee0a6cb1944e1ab60893cf8202192c016b685d3
LOV <LOV@DESKTOP-89BB5UJ>
2021-10-17 14:23:42 +0530

    input 1 , 2  add and returns 3

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -8,22 +8,23 @@ import org.junit.Before;
 
 class StringCalculatorShould {
 	
-	private StringCalculator cal; //commit
-	//comment
-	@Before
-	public void init() {
-		cal = new StringCalculator();
-	}
-
+	
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+	public void twoNumbersReturnsSum() throws Exception
+	{
+    	StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(stringCalculator.add("1,2"), 3);
+	}
 }
