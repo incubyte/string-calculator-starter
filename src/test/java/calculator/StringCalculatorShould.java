6bb9b65b8622de24d502534fd5e49325d80dcd74
PRIYA <PRIYA@DESKTOP-CIV3L73>
2021-10-17 12:43:15 +0530

    Sixth test case added

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class StringCalculatorShould {
 
     @Test
@@ -39,5 +40,20 @@ class StringCalculatorShould {
     void string_with_three_number_and_delimiter_should_return_addition_as_int() {				
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(6, stringCalculator.add("1\n2\n3")); 	//5 test case variant
+    }
+    
+   /* @Test()
+    public void negativeInputReturnsException()
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	Assertions.assertThrows(Exception.class,new StringCalculator());
+    	
+    	stringCalculator.add("-1");
+    }*/
+    
+    @Test
+    public void ignores_numbers_greater_than_thousand() {
+    	StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(20, stringCalculator.add("10,10,1000")); 	//6 test case variant
     }
 }	
