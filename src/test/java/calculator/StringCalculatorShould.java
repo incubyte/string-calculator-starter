0c50ac476402be53b731290986fc605ffd9d6779
Nitesh Ghule <76044162+niteshghule2@users.noreply.github.com>
2021-10-17 12:49:36 +0530

    1st case - empty string => return 0

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	private StringCalculator stringCalculator;
 
-//    @Test
-//    void string_with_single_number_should_return_number_as_int() {
-//        StringCalculator stringCalculator = new StringCalculator();
-//        assertEquals(1, stringCalculator.add("1"));
-//    }
+	@BeforeEach
+	public void init() {
+		stringCalculator = new StringCalculator();
+	}
+
+	@Test
+	void empty_string_should_return_0() {
+		assertEquals(0, stringCalculator.add(""));
+	}
+
 }
