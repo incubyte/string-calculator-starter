98f03c3ff87ffb923501d5df0e0d6487ef61a634
Nitesh Ghule <76044162+niteshghule2@users.noreply.github.com>
2021-10-17 13:04:20 +0530

    2.1st case - string with only one int => return int

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,17 +2,22 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if (input.isEmpty()) {
+	public int add(String input) {
+		if (input.isEmpty()) {
 			return 0;
 		}
-    	return toInt(input);
-    }
-    
-    
- // parse string to integer
- 	private int toInt(String str) {
- 		return Integer.parseInt(str);
- 	}
+		String[] str = input.split(",");
 
+		if (str.length == 1) {
+			return toInt(input);
+		}
+		return -1;
+	}
+
+	// parse string to integer
+	private int toInt(String str) {
+		return Integer.parseInt(str);
+	}
+
+	
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -19,10 +19,11 @@ class StringCalculatorShould {
 	void empty_string_should_return_0() {
 		assertEquals(0, stringCalculator.add(""));
 	}
-	
+
 	@Test
 	void string_with_single_number_should_return_number_as_int() {
 		assertEquals(1, stringCalculator.add("1"));
 	}
 
+	
 }
