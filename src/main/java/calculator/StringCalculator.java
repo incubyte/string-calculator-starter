ab492fc1a1cde6b092849796323db4326730329e
AnuradhaS30 <sangaleanuradha@gmail.com>
2021-10-17 14:59:29 +0530

    add TC 1 : Empty String will return 0

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	// return 0, if String is Empty
+    	if(input.isEmpty()) 
+    		return 0;
+    	
         return 0;
     }
 
