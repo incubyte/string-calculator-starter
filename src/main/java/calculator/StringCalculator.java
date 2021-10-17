8d9572a62d2ec3d504bb5ddc04148167eb166dc4
nilima <nilimamahale29@gmail.com>
2021-10-17 13:20:55 +0530

    Refactored input

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,17 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	if(input.isEmpty())
+    	if(isEmpty(input))
     	{
     		return 0;
     	}
-        return Integer.parseInt(input);
+        return stringToInt(input);
     }
-
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    private int stringToInt(String input) {
+    	return Integer.parseInt(input);
+    }
 }
