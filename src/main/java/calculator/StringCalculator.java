75cf3732e6548479a40c33d8c7c297154117c93f
ZubinVaidya <zubinvaidya@gmail.com>
2021-10-18 11:35:19 +0530

    Fixed a minor flaw in the code and modified the add function to not add numbers bigger than thousand and also handled the case for delimeter of length greater than 1 and also added a test case for the same

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,55 +2,68 @@ package calculator;
 
 class StringCalculator {
 
-    public int compute(String numbers, String delimeter) throws NegativesNotAllowedException {
+    public int compute(String numbers, String delimeter) throws NegativesNotAllowedException{
         String numbersWithoutNewLine = numbers.replaceAll("\\n", delimeter);
-        String numbersWithoutSpace = numbersWithoutNewLine.replaceAll("\\s+", "");
+        String numbersWithoutSpace = numbersWithoutNewLine.replaceAll( "\\s+", "");
         int result = 0;
         int indexOfDelimeter = numbersWithoutSpace.indexOf(delimeter);
         String negatives = "";
-        if (indexOfDelimeter == -1) {
-            if (Integer.parseInt(numbersWithoutSpace) < 0) {
+        if(indexOfDelimeter == -1){
+            if(Integer.parseInt(numbersWithoutSpace) < 0){
                 negatives = numbersWithoutSpace;
             }
-            if (numbersWithoutSpace.isEmpty()) {
-                return Integer.parseInt(numbersWithoutSpace);
-            } else {
+            if(negatives.isEmpty()) {
+                if(Integer.parseInt(numbersWithoutSpace) <= 1000) {
+                    return Integer.parseInt(numbersWithoutSpace);
+                }
+                else{
+                    return 0;
+                }
+            }
+           else{
                 throw new NegativesNotAllowedException("Negatives not allowed " + negatives);
             }
         }
-        while (indexOfDelimeter != -1) {
+        while (indexOfDelimeter != -1){
             int num = Integer.parseInt(numbersWithoutSpace.substring(0, indexOfDelimeter));
             numbersWithoutSpace = numbersWithoutSpace.substring(indexOfDelimeter + 1);
             indexOfDelimeter = numbersWithoutSpace.indexOf(delimeter);
-            result = result + num;
-            if (num < 0) {
+            if(num <= 1000) {
+                result = result + num;
+            }
+            if(num < 0){
                 negatives += num + " ";
             }
         }
-        if (negatives.isEmpty()) {
+        result += Integer.parseInt(numbersWithoutSpace);
+        if(negatives.isEmpty()) {
             return result;
-        } else {
+        }
+        else{
             throw new NegativesNotAllowedException("Negatives not allowed " + negatives);
         }
     }
-
-    public int add(String input) {
-        if (input.isEmpty()) {
+    
+    public int add(String input){
+        if(input.isEmpty()){
             return 0;
         }
-        if (input.substring(0, 2).matches("//")) {
+        if(input.substring(0,2).matches("//")){
             int indexOfNewLine = input.indexOf("\n");
             String delimeter = input.substring(2, indexOfNewLine);
             String numbers = input.substring(indexOfNewLine + 1);
             try {
                 return compute(numbers, delimeter);
-            } catch (NegativesNotAllowedException exception) {
+            }
+            catch (NegativesNotAllowedException exception){
                 System.out.println(exception.getMessage());
             }
-        } else {
+        }
+        else{
             try {
                 return compute(input, ",");
-            } catch (NegativesNotAllowedException exception) {
+            }
+            catch (NegativesNotAllowedException exception){
                 System.out.println(exception.getMessage());
             }
         }
