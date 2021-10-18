ed2782e4b30f3a6dd57203c72aaaa9a54198a1f4
ZubinVaidya <zubinvaidya@gmail.com>
2021-10-18 09:44:10 +0530

    Completed the add function to add two numbers and also written a test case for the same

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,17 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        String numbersWithoutSpace = input.replaceAll("\\s+", "");
+        if (numbersWithoutSpace.isEmpty()) {
+            return 0;
+        }
+        int indexOfComma = numbersWithoutSpace.indexOf(",");
+        if (indexOfComma == -1) {
+            return Integer.parseInt(numbersWithoutSpace);
+        }
+        int num1 = Integer.parseInt(numbersWithoutSpace.substring(0, indexOfComma));
+        int num2 = Integer.parseInt(numbersWithoutSpace.substring(indexOfComma + 1));
+        return num1 + num2;
     }
 
 }
