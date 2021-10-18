238c33d2ef14e8baaf0964ce6fc0b3f74d5a3c9b
Tushar Bobde <84829294+tusharbobde1997@users.noreply.github.com>
2021-10-18 11:20:44 +0530

    initalizing th project src

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,29 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+   if(numbers.isEmpty()) { 
+			return 0;
+		} else {
+			String[] num = splitter(numbers);
+			int size=num.length;
+			throwExceptionIfAnyNegative(num, size);
+			return findSum(num, size);
+		}
+		String[] num = splitter(numbers);
+		int size=num.length;
+		throwExceptionIfAnyNegative(num, size);
+		return findSum(num, size);
+	}
 
-}
+	private static void throwExceptionIfAnyNegative(String[] num, int size) {
+@@ -37,7 +33,9 @@ private static int findSum(String[] num, int size) {
+	}
+
+	private static String[] splitter(String numbers) {
+		if(isCustomDelimiterString(numbers)) {
+		if(numbers.isEmpty()) {
+			return new String[0];
+		} else if(isCustomDelimiterString(numbers)) {
+			return splitUsingCustomDelimiter(numbers);
+		}
+		return splitUsingCommaAndNewLine(numbers);
